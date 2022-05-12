9378cb734d24a5f64caf594cb4c8d517cdca0d67
tushar-jain01 <tushar.kcjain@gmail.com>
2022-05-12 10:24:52 +0530

    About Us Contact us

---
+++ client/src/components/aboutus/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './Banner';
+import Card from 'react-bootstrap/Card'
+
+
+const AboutUs= () => {
+    return (
+        <>
+            <Banner/>
+        </>
+    )
+}
+
+export default AboutUs;
